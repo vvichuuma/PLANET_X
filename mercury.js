a27d5063577ec910d70dd080f241102432658892
vishnu umakanthan <vvichugit@gmail.com>
2019-03-23 16:19:45 -0500

    added mercury

---
+++ mercury.js
@@ -0,0 +1,10 @@
+function mercury(){
+
+
+    alert("Happy journery to mercury.Mercury gets to within 77.3 million km away from the Earth. Light takes only 4.3 minutes to travel from the Earth to Mercury when they’re at their closest point.");
+
+}
+
+function home(){
+	window.location = "home.html";
+}
